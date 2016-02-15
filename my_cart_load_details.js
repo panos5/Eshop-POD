25154b879c7b414f3e9523edca63d22a5d05b0d6
panos5 <panagiotisfiliotis@hotmail.com>
2016-02-15 15:04:07 +0000

    Change styles on css and upload image panel added

--- my_cart_load_details.js
+++ my_cart_load_details.js
@@ -58,3 +58,23 @@ $(document).ready(function () {
     });
 
 });
+
+
+function previewFile(){
+
+    var preview = document.getElementById('profile_image'); //selects the query named img
+    var file    = document.getElementById('image_file_to_upload').files[0]; //sames as here
+    var reader  = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+    } else {
+        preview.src = "";
+    }
+
+}
+
