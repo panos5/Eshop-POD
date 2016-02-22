38ad0c6402efa6501896b038e983ef6fcfce6a69
panos5 <panagiotisfiliotis@hotmail.com>
2016-02-22 12:36:54 +0000

    Check of username on update details updated

--- my_cart_update_details.php
+++ my_cart_update_details.php
@@ -21,7 +21,7 @@
 
         $check_username_exist_sql = mysqli_query($conn , "SELECT user_id FROM register WHERE user_name = '$newUserName'");
 
-        if(mysqli_num_rows($check_username_exist_sql)>0 && $userName != $newUserName)
+        if(strtolower($userName) != $newUserName && mysqli_num_rows($check_username_exist_sql)>0)
         {
 
             $result->status = "nameFailed";
